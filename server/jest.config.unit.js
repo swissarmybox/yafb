491f8063a93dece88eb45a2efa4ceb05ebfe0d89
swissarmybox <swissarmybox@protonmail.ch>
2021-02-16 21:35:56 -0500

    Refactor

--- server/jest.config.unit.js
+++ server/jest.config.unit.js
@@ -3,4 +3,28 @@ const baseJestConfig = require('./jest.config.js')
 module.exports = {
   ...baseJestConfig,
   roots: ['<rootDir>/tests/unit'],
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'reports',
+        outputName: 'unit.test.xml',
+      }
+    ],
+  ],
+  collectCoverageFrom: ['./src','!./node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  coverageDirectory: './reports',
+  coverageReporters: [
+    'text',
+    ['json', { file: 'unit.coverage.json' }],
+  ],
 }
