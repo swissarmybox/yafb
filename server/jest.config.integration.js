5dd51e9e9704391249b8d37c8df2e1518830d310
swissarmybox <swissarmybox@protonmail.ch>
2021-02-16 21:39:59 -0500

    WIP

--- server/jest.config.integration.js
+++ server/jest.config.integration.js
@@ -4,28 +4,28 @@ module.exports = {
   ...baseJestConfig,
   roots: ['<rootDir>/tests/integration'],
   setupFilesAfterEnv: ['<rootDir>/tests/integration/setup.ts'],
-  // collectCoverageFrom: ['./','!./node_modules/'],
-  // coverageTreshold: {
-  //   global: {
-  //     branches: 0,
-  //     functions: 0,
-  //     lines: 0,
-  //     statements: 0,
-  //   },
-  // },
-  // coverageDirectory: './reports/coverage',
-  // coverageResporters: [
-  //   'text',
-  //   ['json', { file: 'api-sytem.json' }],
-  // ],
-  // repoters: [
-  //   'default',
-  //   [
-  //     'jest-junit',
-  //     {
-  //       outputDirectory: 'reportes/test',
-  //       outputName: 'api.system.test.xml',
-  //     }
-  //   ],
-  // ]
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      {
+        outputDirectory: 'reports',
+        outputName: 'integration.test.xml',
+      }
+    ],
+  ],
+  collectCoverageFrom: ['./src','!./node_modules/'],
+  coverageThreshold: {
+    global: {
+      branches: 0,
+      functions: 0,
+      lines: 0,
+      statements: 0,
+    },
+  },
+  coverageDirectory: './reports',
+  coverageReporters: [
+    'text',
+    ['json', { file: 'integration.coverage.json' }],
+  ],
 }
